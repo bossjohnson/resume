e7f494dcac978738abfffbcd402509bd55cb8b2e
Josh Newsom <newsommeister@gmail.com>
2016-08-20 22:59:39 -0600

    Add favicon

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
